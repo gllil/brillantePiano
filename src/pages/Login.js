7188c7fdb897a930d3bfa9f2529208507941a24d
unknown <calebhaabbott94@gmail.com>
2021-08-04 16:49:52 -0400

    Login form

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,31 @@
+import { Button, Col, Container, Form, Row } from 'react-bootstrap';
+
 const Login = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Container className='mb-5 mt-5'>
+        <Row className='text-center'>
+          <h2>Login</h2>
+        </Row>
+
+        <Row className='justify-content-center'>
+          <Col xs={12} md={6}>
+            <Form>
+              <Form.Group>
+                <Form.Label>Email</Form.Label>
+                <Form.Control type='email' name='email' required />
+              </Form.Group>
+              <Form.Group>
+                <Form.Label>Password</Form.Label>
+                <Form.Control type='password' name='password' required />
+              </Form.Group>
+              <Button className='mt-3'>Login</Button>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 };
 
 export default Login;
