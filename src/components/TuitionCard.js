5c8dd8261ee906dfd4b8a435a776c52b3cae3546
Gary Llil <garyjllil@gmail.com>
2022-01-18 21:01:17 -0500

    fixed styling issues with button and badges

--- src/components/TuitionCard.js
+++ src/components/TuitionCard.js
@@ -83,7 +83,9 @@ const TuitionCard = ({
     <Col xs={12} sm={6} xl={3} className="mt-4">
       <Card className="tuitionCards">
         <h2 className="text-center badgeWrapper">
-          <Badge pill>{cardBadge}</Badge>
+          <Badge bg="dark" pill>
+            {cardBadge}
+          </Badge>
         </h2>
         <Card.Body className="align-items-center">
           <Card.Title as="h3">{titleOne}</Card.Title>
